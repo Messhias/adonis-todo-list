dd9543b1941eb606ecc412a357744ec39e44737c
Fabio William Conceição <messhias@gmail.com>
2019-11-11 20:00:17 +0400

    Updates 2019-11-11
    - Created migrations
    - General migrations

---
+++ database/migrations/1573487242086_todo_list_schema.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class TodoListSchema extends Schema {
+    up () {
+        this.create('todo_lists', (table) => {
+            table.increments();
+            table.integer('user_id').unsigned().references('id').inTable('users');
+            table.string("name", 254);
+            table.boolean("status").default(1);
+            table.timestamps();
+        })
+    }
+
+    down () {
+        this.drop('todo_lists');
+    }
+}
+
+module.exports = TodoListSchema;
