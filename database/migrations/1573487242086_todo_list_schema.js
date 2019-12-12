444b9c6612c9b9eb44bc2b03d7fa8b539a698bd1
Fabio William Conceição <fabio@viame.ae>
2019-12-12 23:54:10 +0400

    Default project configurations updates Updated the project identation.

--- database/migrations/1573487242086_todo_list_schema.js
+++ database/migrations/1573487242086_todo_list_schema.js
@@ -4,19 +4,19 @@
 const Schema = use('Schema');
 
 class TodoListSchema extends Schema {
-    up () {
-        this.create('todo_lists', (table) => {
-            table.increments();
-            table.integer('user_id').unsigned().references('id').inTable('users');
-            table.string("name", 254);
-            table.boolean("status").default(1);
-            table.timestamps();
-        })
-    }
+	up () {
+		this.create('todo_lists', (table) => {
+			table.increments();
+			table.integer('user_id').unsigned().references('id').inTable('users');
+			table.string("name", 254);
+			table.boolean("status").default(1);
+			table.timestamps();
+		});
+	}
 
-    down () {
-        this.drop('todo_lists');
-    }
+	down () {
+		this.drop('todo_lists');
+	}
 }
 
 module.exports = TodoListSchema;
